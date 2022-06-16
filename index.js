f12f3749a3bae136b0dc15dba17d9ec91c4da5b8
ProgramerPritom <badruzzamanpritom@gmail.com>
2022-06-16 19:53:42 +0600

    mongodb added in server site

--- index.js
+++ index.js
@@ -1,6 +1,43 @@
 const express = require('express')
 const app = express()
+require('dotenv').config()
+const cors = require('cors')
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const port = process.env.PORT || 5000;
+
+// middleware 
+app.use(cors())
+app.use(express.json())
+
+/**
+ * username:gymEquipDb
+ * password:j7OZ0wMRbx4nzLW7
+ */
+
+
+ 
+ const uri = `mongodb+srv://${process.env.DB_NAME}:${process.env.DB_PASSWORD}@cluster0.kotkn.mongodb.net/?retryWrites=true&w=majority`;
+ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+
+ async function run() {
+    try {
+          await client.connect();
+          const collection = client.db("GymEquipment").collection("Equipment");
+
+      
+    } finally {
+      
+    }
+    run().catch(console.dir);
+
+
+
+ }
+
+
+ 
 
 
 app.get('/', (req, res) => {
@@ -8,5 +45,5 @@ app.get('/', (req, res) => {
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`Running Gym Equipment port is ${port}`)
 })
